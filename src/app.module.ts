6a47a872634c3b41520679aed2e183682cacd908
thiagoa261 <thiagoa371@gmail.com>
2026-04-29 23:08:33 -0300

    feat: JWT adicionado

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from "./prisma/prisma.module";
+import { ModulesModule } from "./modules/modules.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-	imports: [ConfigModule.forRoot(), PrismaModule],
+	imports: [
+		ConfigModule.forRoot(),
+		JwtModule.register({
+			global: true,
+			secret: process.env.JWT_SECRET,
+			signOptions: { expiresIn: "8h" },
+		}),
+		PrismaModule,
+		ModulesModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
