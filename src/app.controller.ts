7af3e38530f1ada2a69b0ef847538e116c79a108
thiagoa261 <thiagoa371@gmail.com>
2026-04-29 21:40:48 -0300

    feat: conexão com o banco

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
 	constructor(private readonly appService: AppService) {}
 
 	@Get()
-	getHello(): string {
-		return this.appService.getHello();
+	apiOnline(): string {
+		return this.appService.apiOnline();
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import morgan from "morgan";
 
 async function bootstrap() {
+	if (!process.env.PORT) throw new Error("PORT não definido no .env");
+	if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL não definido no .env");
+
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+
+	app.enableCors();
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+		}),
+	);
+
+	app.use(morgan("dev"));
+
+	await app.listen(process.env.PORT);
+
+	console.log(`🚀 aplicação rodando em: http://localhost:${process.env.PORT}`);
 }
+
 bootstrap();
