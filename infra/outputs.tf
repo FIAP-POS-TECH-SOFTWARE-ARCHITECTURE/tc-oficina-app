11f58931aac8fdec63f70ee6a9d14c61fd8618ae
Lucas-Gardini <lucasgardini.dias@hotmail.com>
2026-07-09 20:43:23 -0300

    feat(infra): rds postgres, ecr e script de credenciais do academy

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -9,3 +9,16 @@ output "cluster_endpoint" {
 output "configure_kubectl" {
   value = "aws eks update-kubeconfig --region ${var.aws_region} --name ${aws_eks_cluster.main.name}"
 }
+
+output "rds_endpoint" {
+  value = aws_db_instance.main.address
+}
+
+output "database_url" {
+  value     = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.main.address}:5432/${var.db_name}"
+  sensitive = true
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.api.repository_url
+}
