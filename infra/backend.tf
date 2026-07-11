39e88dbf15916419c5959cafe43e3757499d5a24
Lucas-Gardini <lucasgardini.dias@hotmail.com>
2026-07-10 21:42:46 -0300

    chore(infra): fixa bucket do tfstate com id da conta AWS Academy

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,7 +3,7 @@
 #   aws s3api put-bucket-versioning --bucket <SEU_BUCKET_TFSTATE> --versioning-configuration Status=Enabled
 terraform {
   backend "s3" {
-    bucket       = "tc-fiap-oficina-tfstate" # ajustar: nome de bucket é global — sufixar com id da conta
+    bucket       = "tc-fiap-oficina-tfstate-076155200589" # ajustar: nome de bucket é global — sufixar com id da conta
     key          = "fase-2/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
